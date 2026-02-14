5f3e6d1b0767db6411d4531e532ef183684d2db8
phos-x <43341466+phos-x@users.noreply.github.com>
2026-02-14 07:37:03 +0100

    corrected time zone

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,7 +8,7 @@ const person: Person = {
   role: "Platform Engineer",
   avatar: "/images/avatar.jpg",
   email: "adetayoadebayo67@gmail.com",
-  location: "Europe/Glasgow", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Europe/London", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Yoruba"], // optional: Leave the array empty if you don't want to display languages
 };
 
